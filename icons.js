7002dac62373df45cf09c8f3303481308a1ac943
Skyler Berry <skylerberry@Skylers-MacBook-Pro.local>
2026-08-18 16:03:11 -0700

    Pair a coffee mark with the footer heart.
    made with [heart] [coffee] by skyler. Coffee is mocha on light,
    warm tan on dark, and caramel on OLED.

--- icons.js
+++ icons.js
@@ -53,6 +53,7 @@ const ICONS = (() => {
         'external-link': w('<path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>'),
         'message-square': w('<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>'),
         heart: w('<path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" fill="currentColor" stroke="none"/>'),
+        coffee: w('<path d="M10 2v2"/><path d="M14 2v2"/><path d="M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1"/><path d="M6 2v2"/>'),
     };
 })();
 
